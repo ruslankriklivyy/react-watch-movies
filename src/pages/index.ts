75c21f2b7abef55e43c4cd53bf0bf4a93bcac522
ruslankriklivyy <ruslankriklivyy@gmail.com>
2021-03-25 10:44:51 +0200

    Add typescript

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Movies } from './Movies';
 export { default as Home } from './Home';
 export { default as MovieItem } from './MovieItem';
+export { default as AppInitialize } from './AppInitialize';
